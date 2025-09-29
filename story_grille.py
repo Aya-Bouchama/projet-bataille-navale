df2564a30aeda79e1154f907a95fdadc500a608d
Aya-Bouchama <bouchamaaya34@gmail.com>
2025-09-29 10:54:52 +0200

    Implémentation de la User Story 'Plouf dans l'eau'

--- story_grille.py
+++ story_grille.py
@@ -1,37 +1,13 @@
-"""
-User Story : "Plouf dans l'eau"
-Utilisateur : un joueur
-Story : On veut pouvoir gérer les tirs de l'adversaire
-Actions :
-1. créer une grille à 5 lignes et 8 colonnes
-2. afficher la grille à l'écran
-3. demander à l'utilisateur de rentrer deux coordonnées x et y
-4. tirer à l'endroit indiqué sur la grille
-5. retour en 2
-"""
-
-
-
 from src.grille import Grille  
 
 def main():
-    # 1. Créer une grille de 5 lignes et 8 colonnes
-    ma_grille = Grille(5, 8)   # constructeur à coder dans grille.py
+    ma_grille = Grille(5, 8)
 
     while True:
-        # 2. Afficher la grille
-        ma_grille.afficher()   # méthode à coder dans Grille
-
-        # 3. Demander à l’utilisateur des coordonnées
-        print("Entrez les coordonnées (x y) : ")
-        x, y = map(int, input().split())
-
-        # 4. Tirer sur la grille
-        resultat = ma_grille.tirer(x, y)   # méthode à coder dans Grille
-        print("Résultat :", resultat)
-
-        # 5. Retour en 2 (la boucle while reprend)
-
+        print(ma_grille)
+        print("Entrez les coordonnées (ligne colonne) : ")
+        ligne, colonne = map(int, input().split())
+        ma_grille.tirer(ligne, colonne)
 
 if __name__ == "__main__":
     main()
